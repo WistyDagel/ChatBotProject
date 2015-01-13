eb7ac1c596ab6f5567b033a5edc67f216af9c699
WistyDagel <chrisdazley@gmail.com>
2015-01-12 17:20:25 -0700

    Documentation

--- src/chat/view/ChatBotView.java
+++ src/chat/view/ChatBotView.java
@@ -4,16 +4,31 @@ import javax.swing.JOptionPane;
 
 import chat.controller.ChatBotController;
 
+/**
+* Allows th user to view the ChatBot. Used for the GUI of the Chatbot
+* @author Chris D
+*
+*/
+
 public class ChatBotView 
 {
-
+	/**
+	 * Model components can be accessed for display purposes.
+	 */
 	private ChatBotController baseController; 
-	
+	/**
+	 * Creates the ChatbotView with a reference to the controller.
+	 * @param The reference to the ChatbotController.
+	 */
 	public ChatBotView(ChatBotController baseController)
 	{
 		this.baseController = baseController; 
 	}
-	 
+	/**
+	 * String from the Chatbot with the availability of user input.
+	 * @param currentInput The used String.
+	 * @return The users typed response.
+	 */
 	public String displayChatBotConversations(String input)
 	{
 		String output = "";
@@ -22,7 +37,10 @@ public class ChatBotView
 		
 		return output;
 	}
-	
+	/**
+	 * Shows a String from the Chatbot as a popup window.
+	 * @param currentInput The string from the Chatbot.
+	 */
 	public void displayInformation(String input)
 	{
 		JOptionPane.showMessageDialog(null, input);
