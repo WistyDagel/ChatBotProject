3b9fff5c2faa35aa5c1537ec32da5f3e157f6ece
WistyDagel <chrisdazley@gmail.com>
2015-01-12 17:11:18 -0700

    Added Documentation

--- src/chat/view/ChatBotFrame.java
+++ src/chat/view/ChatBotFrame.java
@@ -6,16 +6,30 @@ import chat.controller.ChatBotController;
 
 import chat.controller.ChatBotRunner;
 
+/**
+* GUI Frame class for a Chatbot. Shows the basic framework code for a JFrame extension.
+* @author Chris Dazley
+* @version 
+*/
+
 public class ChatBotFrame extends JFrame
 {
+	/**
+	 * Reference for the ChatbotPanel of the GUI.
+	 */
 	private ChatBotPanel basePanel;
-	
+	/**
+	 * Creates a ChatbotFrame with a reference to the ChatbotAppController to pass to the panel.
+	 * @param baseController The reference to the controller object.
+	 */
 	public ChatBotFrame(ChatBotController baseController) 
 	{
 		basePanel = new ChatBotPanel(baseController);
 		setupFrame();
 	}
-	
+	/**
+	 * Helper method to load panel to set size, frame and visibility. 
+	 */
 	private void setupFrame()
 	{
 		this.setContentPane(basePanel);
